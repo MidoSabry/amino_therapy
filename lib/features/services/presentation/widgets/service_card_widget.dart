24f95e09ca06a0b3d01621742eed3cd685c6b607
msramadam <msramadan@2p.com.sa>
2025-10-28 09:35:56 +0300

    change text button color

--- lib/features/services/presentation/widgets/service_card_widget.dart
+++ lib/features/services/presentation/widgets/service_card_widget.dart
@@ -1,5 +1,10 @@
+import 'package:amino_therapy/core/component/button/p_button.dart';
+import 'package:amino_therapy/core/component/text/p_text.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
+import '../../../../core/data/constants/app_colors.dart';
+import '../../../../core/global/enums/global_enum.dart';
 import '../../data/service_model.dart';
 
 class ServiceCard extends StatelessWidget {
@@ -16,29 +21,61 @@ class ServiceCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(service.name, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            Text(service.description, style: const TextStyle(color: Colors.black54)),
+            PText(
+              title: service.name,
+              size: PSize.text14,
+              fontWeight: FontWeight.bold,
+            ),
+            PText(
+              title: service.description,
+              fontColor: AppColors.black,
+              size: PSize.text14,
+            ),
             const SizedBox(height: 8),
             Row(
               children: [
-                const Icon(Icons.star, color: Colors.redAccent, size: 18),
+                const Icon(Icons.star, color: AppColors.primaryColor, size: 18),
                 const SizedBox(width: 4),
-                Text("${service.rating} (${service.reviewsCount} reviews)"),
+                PText(
+                  title: "${service.rating} (${service.reviewsCount} reviews)",
+                  size: PSize.text14,
+                  fontColor: AppColors.black,
+                ),
               ],
             ),
             const SizedBox(height: 8),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("\$${service.price}", style: const TextStyle(fontSize: 20, color: Colors.redAccent)),
-                Text(service.duration),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.redAccent,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                PText(
+                  title: "\$${service.price}",
+                  size: PSize.text16,
+                  fontWeight: FontWeight.bold,
+                  fontColor: AppColors.primaryColor,
+                ),
+                PText(title: service.duration, size: PSize.text14),
+                // ElevatedButton(
+                //   style: ElevatedButton.styleFrom(
+                //     backgroundColor: Colors.redAccent,
+                //     shape: RoundedRectangleBorder(
+                //       borderRadius: BorderRadius.circular(12),
+                //     ),
+                //   ),
+                //   onPressed: () {},
+                //   child: const Text(
+                //     "Book",
+                //     style: TextStyle(color: Colors.white),
+                //   ),
+                // ),
+                SizedBox(
+                  width: 25.w,
+                  child: PRoundedButton(
+                    borderRadius: 15,
+                    onPressed: () {},
+                    title: "Book",
+                    size: PSize.text14,
+                    textColor: AppColors.whiteColor,
                   ),
-                  onPressed: () {},
-                  child: const Text("Book"),
                 ),
               ],
             ),
